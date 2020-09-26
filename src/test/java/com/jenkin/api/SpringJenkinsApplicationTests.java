0e9f7873cc19c2a6b1d350f1b6298dc27a75bb5c
pradiptamishra <mishra.pradipta@gmail.com>
2020-09-26 13:05:59 -0400

    first commit

---
+++ src/test/java/com/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger  logger=LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	@Test
+	public void contextLoads() {
+		
+		logger.info("Application started......");		
+		assertEquals(true, true);
+	}
+
+}
